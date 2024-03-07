085281617c6bfb242e2940c4ee840829ff4e0177
Simon Tartakovsky <simtart@gmail.com>
2024-03-07 18:11:05 -0500

    working... nise

--- src/lib.rs
+++ src/lib.rs
@@ -9,60 +9,51 @@ pub mod ffi {
 mod tests;
 
 use std::any::TypeId;
+use std::error;
 use std::ffi::CStr;
-use std::ffi::CString; //use this to go from rust to c
-use std::os::fd::RawFd; //use this to go from c to rust
+use std::ffi::CString;
+
 
 //lets make a struct to hold the dirfile
 pub struct Dirfile {
     pub dirfile: Option<std::ptr::NonNull<ffi::DIRFILE>>,
 }
-
+#[derive(Clone, Copy)]
 pub enum GdTypes {
     Float32,
     Float64,
     Int32,
     Int64,
 }
-pub trait FromRaw {
-    unsafe fn from_raw(raw: *const std::ffi::c_void, len: usize) -> Self;
-}
 
-impl FromRaw for Vec<f32> {
-    unsafe fn from_raw(raw: *const std::ffi::c_void, len: usize) -> Self {
-        std::slice::from_raw_parts(raw as *const f32, len).to_vec()
-    }
-}
-impl FromRaw for Vec<f64> {
-    unsafe fn from_raw(raw: *const std::ffi::c_void, len: usize) -> Self {
-        std::slice::from_raw_parts(raw as *const f64, len).to_vec()
-    }
-}
-
-impl FromRaw for Vec<i32> {
-    unsafe fn from_raw(raw: *const std::ffi::c_void, len: usize) -> Self {
-        std::slice::from_raw_parts(raw as *const i32, len).to_vec()
-    }
-}
-
-impl FromRaw for Vec<i64> {
-    unsafe fn from_raw(raw: *const std::ffi::c_void, len: usize) -> Self {
-        std::slice::from_raw_parts(raw as *const i64, len).to_vec()
-    }
-}
-
-
-impl GdTypes {
-    fn to_ffi(&self) -> ffi::gd_type_t {
-        match self {
+impl From<GdTypes> for ffi::gd_type_t {
+    fn from(gd_type: GdTypes) -> Self {
+        match gd_type {
             GdTypes::Float32 => ffi::gd_type_t_GD_FLOAT32,
             GdTypes::Float64 => ffi::gd_type_t_GD_FLOAT64,
             GdTypes::Int32 => ffi::gd_type_t_GD_INT32,
             GdTypes::Int64 => ffi::gd_type_t_GD_INT64,
         }
     }
-    fn type_id(&self) -> TypeId {
-        match self {
+}
+
+impl From<ffi::gd_type_t> for GdTypes {
+    fn from(gd_type: ffi::gd_type_t) -> Self {
+        match gd_type {
+            ffi::gd_type_t_GD_FLOAT32 => GdTypes::Float32,
+            ffi::gd_type_t_GD_FLOAT64 => GdTypes::Float64,
+            ffi::gd_type_t_GD_INT32 => GdTypes::Int32,
+            ffi::gd_type_t_GD_INT64 => GdTypes::Int64,
+            _ => {
+                panic!("Unsupported type");
+            }
+        }
+    }
+}
+
+impl From<GdTypes> for TypeId {
+    fn from(gd_type: GdTypes) -> Self {
+        match gd_type {
             GdTypes::Float32 => TypeId::of::<f32>(),
             GdTypes::Float64 => TypeId::of::<f64>(),
             GdTypes::Int32 => TypeId::of::<i32>(),
@@ -71,60 +62,190 @@ impl GdTypes {
     }
 }
 
+#[derive(Clone, Copy)]
 pub enum EntryType {
-    Raw { data_type: GdTypes, spf: u32 },
+    Raw {
+        data_type: Option<GdTypes>,
+        spf: Option<u32>,
+    },
+    // Add other variants here
 }
+
+impl From<EntryType> for ffi::gd_entype_t {
+    fn from(entry_type: EntryType) -> Self {
+        match entry_type {
+            EntryType::Raw { .. } => ffi::gd_entype_t_GD_RAW_ENTRY,
+            // Add other variants here
+        }
+    }
+}
+
+impl From<ffi::gd_entype_t> for EntryType {
+    fn from(entry_type: ffi::gd_entype_t) -> Self {
+        match entry_type {
+            ffi::gd_entype_t_GD_RAW_ENTRY => EntryType::Raw {
+                data_type: None,
+                spf: None,
+            },
+            _ => {
+                panic!("Unsupported entry type");
+            }
+        }
+    }
+}
+impl EntryType {
+    pub fn new_raw(data_type: GdTypes, spf: u32) -> Self {
+        EntryType::Raw {
+            data_type: Some(data_type),
+            spf: Some(spf),
+        }
+    }
+}
+
 pub struct Entry {
     pub field_code: String,
     pub fragment_index: i32,
     pub entry_type: EntryType,
+    entry_c: ffi::gd_entry_t,
+    _field_code_c: CString,
+}
+
+impl Entry {
+    pub fn new(field_code: &str, fragment_index: i32, entry_type: EntryType) -> Entry {
+        let field_code_c = CString::new(field_code).unwrap();
+        let mut entry_c: ffi::gd_entry_t;
+        unsafe {
+            entry_c = std::mem::zeroed();
+        }
+        //assign the field name
+        entry_c.field = field_code_c.as_ptr() as *mut i8;
+        match entry_type {
+            EntryType::Raw { data_type, spf } => {
+                entry_c.field_type = entry_type.into();
+                entry_c.__bindgen_anon_1.__bindgen_anon_1.data_type = data_type.unwrap().into();
+                entry_c.__bindgen_anon_1.__bindgen_anon_1.spf = spf.unwrap();
+            }
+        }
+        Entry {
+            field_code: field_code.to_string(),
+            fragment_index: fragment_index,
+            entry_type: entry_type,
+            entry_c: entry_c,
+            _field_code_c: field_code_c,
+        }
+    }
+    pub fn from_c(field_code: &str, fragment_index: i32, entry_c: ffi::gd_entry_t) -> Entry {
+        let field_code_c: CString = CString::new(field_code).unwrap();
+
+        let entry_type: EntryType = unsafe {
+            match EntryType::from(entry_c.field_type) {
+                EntryType::Raw { .. } => EntryType::Raw {
+                    data_type: Some(GdTypes::from(
+                        entry_c.__bindgen_anon_1.__bindgen_anon_1.data_type,
+                    )),
+                    spf: Some(entry_c.__bindgen_anon_1.__bindgen_anon_1.spf),
+                },
+            }
+        };
+        Entry {
+            field_code: field_code.to_string(),
+            fragment_index: fragment_index,
+            entry_type: entry_type,
+            entry_c: entry_c,
+            _field_code_c: field_code_c,
+        }
+    }
+}
+
+pub enum FieldOrEntry {
+    Field(String),
+    Entry(Entry),
 }
 
 impl Dirfile {
     /// Open a dirfile in read/write mode, creating it if it does not exist
-    pub fn open(dirfile_name: &str) -> Dirfile {
+    pub fn open(dirfile_name: &str) -> Result<Dirfile, String> {
         let dirfile_name = CString::new(dirfile_name).unwrap();
         let dirfile =
             unsafe { ffi::gd_open(dirfile_name.as_ptr(), (ffi::GD_RDWR | ffi::GD_CREAT).into()) };
-        Dirfile {
+        let df = Dirfile {
             dirfile: std::ptr::NonNull::new(dirfile),
+        };
+        match df.get_error() {
+            None => Ok(Dirfile {
+                dirfile: std::ptr::NonNull::new(dirfile),
+            }),
+            Some(error) => { 
+                unsafe { ffi::gd_close(dirfile) };
+                Err(error)
+            },
         }
+        
     }
     /// Close the dirfile
     pub fn close(&mut self) {
-            unsafe { ffi::gd_close(self.dirfile.expect("open dirfile!").as_ptr()) };
-            self.dirfile = None;
+        unsafe { ffi::gd_close(self.dirfile.expect("open dirfile!").as_ptr()) };
+        self.dirfile = None;
     }
 
     /// add entry
-    pub fn add(&mut self, entry: Entry) {
-        match entry.entry_type {
-            //only raw data is supported for now
-            EntryType::Raw { data_type, spf } => {
-                //create a c string
-                let field_code = CString::new(entry.field_code).unwrap();
-                let ret_val = unsafe {
-                    ffi::gd_add_raw(
-                        self.dirfile.expect("open the dirfile!").as_ptr(),
-                        field_code.as_ptr(),
-                        data_type.to_ffi(),
-                        spf,
-                        0,
-                    )
-                };
-                if ret_val != 0 {
-                    panic!("gd_add_raw failed with error: {}", ret_val);
-                }
-            }
+    pub fn add(&mut self, entry: &Entry) -> Result<(), String> {
+        let ret_val = unsafe {
+            ffi::gd_add(
+                self.dirfile.expect("Open the dirfile!").as_ptr(),
+                &entry.entry_c,
+            )
+        };
+        if ret_val == 0 {
+            Ok(())
+        } else {
+            Err(self.get_error().unwrap())
         }
     }
+
+    pub fn get_entry(&self, field_code: &str) -> Result<Entry, String> {
+        let field_code = CString::new(field_code).unwrap();
+        let mut entry_c: ffi::gd_entry_t;
+        unsafe {
+            entry_c = std::mem::zeroed();
+        }
+        let ret_val = unsafe {
+            ffi::gd_entry(
+                self.dirfile.expect("Open the dirfile!").as_ptr(),
+                field_code.as_ptr(),
+                &mut entry_c,
+            )
+        };
+        if ret_val != 0 {
+            return Err(self.get_error().unwrap());
+        }
+
+        let entry = Entry::from_c(field_code.to_str().unwrap(), 0, entry_c);
+        Ok(entry)
+    }
+
     /// puts data vectors, returns if the write was successful
-    pub fn putdata<T: 'static>(&mut self, entry: Entry, data: &Vec<T>) -> Result<usize, &'static str> {
+    pub fn putdata<T: 'static>(
+        &mut self,
+        field_or_entry: FieldOrEntry,
+        data: &Vec<T>,
+    ) -> Result<usize, String> {
+        let entry = match field_or_entry {
+            FieldOrEntry::Field(field_code) => self.get_entry(&field_code).unwrap(),
+            FieldOrEntry::Entry(entry) => entry,
+        };
         match entry.entry_type {
             //only raw data is supported for now
             EntryType::Raw { data_type, spf } => {
                 //check that the type is correct
-                assert_eq!(data_type.type_id(), TypeId::of::<T>(), "Data type mismatch");
+                assert_eq!(
+                    TypeId::from(data_type.unwrap()),
+                    TypeId::of::<T>(),
+                    "Data type mismatch"
+                );
+                //figure out how much data to write
+                let num_frames: usize = data.len()/spf.unwrap() as usize;
+                let num_samples: usize = data.len()%spf.unwrap() as usize;
                 //create a c string
                 let field_code = CString::new(entry.field_code).unwrap();
                 //write data, will need to update the offset and stuff...
@@ -132,42 +253,56 @@ impl Dirfile {
                     ffi::gd_putdata(
                         self.dirfile.expect("Open the dirfile!").as_ptr(),
                         field_code.as_ptr(),
+                        ffi::GD_HERE.into(),
                         0,
-                        0,
-                        data.len() as usize,
-                        0,
-                        data_type.to_ffi(),
+                        num_frames,
+                        num_samples,
+                        data_type.unwrap().into(),
                         data.as_ptr() as *const std::ffi::c_void,
                     )
                 };
-                if write_n != data.len(){
-                    return Err("gd_putdata failed");
+                if write_n != data.len() {
+                    match self.get_error() {
+                        Some(error) => return Err(error),
+                        None => {},
+                    };
                 }
                 Ok(write_n)
             }
         }
     }
 
-    pub fn get_entry(&mut self, field_code: &str) -> Option<Entry> {
-        let field_code = CString::new(field_code).unwrap();
-        let mut c_entry: *mut ffi::gd_unified_entry_;
-        let ret = unsafe {
-            ffi::gd_entry(self.dirfile.unwrap().as_ptr(), field_code.as_ptr(),c_entry)
-        };
-        if (ret != 0) {
+    pub fn flush(&mut self) -> Result<(), String> {
+        let ret_val = unsafe { ffi::gd_flush(self.dirfile.unwrap().as_ptr(), std::ptr::null_mut()) };
+        if ret_val != 0 {
+            return Err(self.get_error().unwrap());
+        }
+        Ok(())
+    }
+    pub fn sync(&mut self) -> Result<(), String> {
+        let ret_val = unsafe { ffi::gd_sync(self.dirfile.unwrap().as_ptr(), std::ptr::null_mut()) };
+        if ret_val != 0 {
+            return Err(self.get_error().unwrap());
+        }
+        Ok(())
+    }
+    pub fn metaflush(&mut self) -> Result<(), String> {
+        let ret_val = unsafe { ffi::gd_metaflush(self.dirfile.unwrap().as_ptr()) };
+        if ret_val != 0 {
+            return Err(self.get_error().unwrap());
+        }
+        Ok(())
+    }
+
+    pub fn get_error(&self) -> Option<String> {
+        let error = unsafe { ffi::gd_error(self.dirfile.unwrap().as_ptr()) };
+        if error == ffi::GD_E_OK as i32 {
             return None;
         }
-
-        //need to convert from the c entry to the rust entry
-        let entry = unsafe { *c_entry };
-        entry.data_type;
-
-
-    }
-        
-
-    ///getdata
-    pub fn getdata<T: FromRaw>(&mut self, field_code: &str, first_frame: usize, first_sample: usize, num_frames: usize, num_samples: usize) -> Result<T,&'static str>{
-        Ok(Vec::new())
+        let error_string_ptr = unsafe { ffi::gd_error_string(self.dirfile.unwrap().as_ptr(), std::ptr::null_mut(), 0) };
+    let error_string_cstr: &CStr = unsafe { CStr::from_ptr(error_string_ptr) };
+    let error_string = error_string_cstr.to_string_lossy().into_owned();
+    unsafe { libc::free(error_string_ptr as *mut libc::c_void) };  // Free the error string
+    Some(error_string)
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -78,11 +78,87 @@ fn test_highlevel_open_close() {
     if path.exists() {
         std::fs::remove_dir_all(file_name).unwrap();
     }
-    let mut dirfile = super::Dirfile::open(file_name);
+    let mut dirfile = super::Dirfile::open(file_name).unwrap();
     dirfile.close();
     //check for the existance of the folder
     let path = std::path::Path::new(file_name);
     assert!(path.exists());
     // //delete the folder
     std::fs::remove_dir_all(file_name).unwrap();
+}
+
+#[test]
+fn test_highlevel_add_entry(){
+    use super::*;
+    let file_name = "__testdirfile4__";
+    let path = std::path::Path::new(file_name);
+    if path.exists() {
+        std::fs::remove_dir_all(file_name).unwrap();
+    }
+    let mut dirfile = super::Dirfile::open(file_name).unwrap();
+    let entry = Entry::new("testfield", 0, EntryType::new_raw(GdTypes::Float32, 10));
+    dirfile.add(&entry).unwrap();
+    dirfile.close();
+    // //check for the existance of the folder
+    let path = std::path::Path::new(file_name);
+    assert!(path.exists());
+
+    // there should be a format file inside the folder which contains 
+    // /VERSION 10
+    // /ENDIAN little
+    // /PROTECT none
+    // /ENCODING none
+    // testfield RAW FLOAT32 10
+    // /REFERENCE testfield
+    // this will be somewhere in that file but not at the top
+    let format_file = std::fs::read_to_string(format!("{}/format", file_name)).unwrap();
+    assert!(format_file.contains("testfield RAW FLOAT32 10"));
+    assert!(format_file.contains("/REFERENCE testfield")); 
+
+    let mut dirfile = Dirfile::open(file_name).unwrap();
+    let entry = dirfile.get_entry("testfield").unwrap();
+    assert_eq!(entry.field_code, "testfield");
+
+    //try to put data!
+    let npoint = 33;
+    let data: Vec<f32> = vec![42.0; npoint];
+    dirfile.putdata(FieldOrEntry::Field("testfield".to_string()), &data).unwrap();
+    dirfile.putdata(FieldOrEntry::Field("testfield".to_string()), &data).unwrap();
+    dirfile.putdata(FieldOrEntry::Field("testfield".to_string()), &data).unwrap();
+
+    dirfile.close();
+
+    //open again to read and double check that it works
+    let mut dirfile = Dirfile::open(file_name).unwrap();
+    let mut data_read: Vec<f32> = vec![0.0; npoint];
+    let fc = CString::new("testfield").unwrap();
+    let n_pts = unsafe {ffi::gd_getdata(dirfile.dirfile.unwrap().as_ptr(), fc.as_ptr(), 0, 0, 3, 3, gd_type_t_GD_FLOAT32, data_read.as_mut_ptr() as *mut c_void) };
+    assert!(n_pts == npoint);
+    assert_eq!(data, data_read);
+    dirfile.close();
+
+
+
+    //delete the folder
+    std::fs::remove_dir_all(file_name).unwrap();
+}
+
+
+#[test]
+fn test_highlevel_error(){
+    //lets try to read from a field that does not exist
+    let file_name = "__fakedirfilenoexist_";
+    let dirfile = super::Dirfile::open(file_name);
+    //try to read from it
+    let mut data = vec![0.0; 10];
+    let dirfile = dirfile.unwrap();
+    unsafe{
+    let fieldcode = CString::new("testfield").unwrap();
+    let n_pts = gd_getdata(dirfile.dirfile.unwrap().as_ptr(), fieldcode.as_ptr(), 0, 0, 10, 0, gd_type_t_GD_FLOAT32, data.as_mut_ptr() as *mut c_void);
+    }
+    let er = dirfile.get_error();
+    assert_eq!(er, Some("Field not found: testfield".to_string()));
+
+    std::fs::remove_dir_all(file_name).unwrap();
+
 }
